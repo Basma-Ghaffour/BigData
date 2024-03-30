3f2b3a4870ec933ee0dbce54243b50a648a86c45
Basma-G <127091566+Basma-Ghaffour@users.noreply.github.com>
2024-03-30 13:19:33 +0100

    Update 3_Base_de_Donnée.py

--- pages/3_Base_de_Donnée.py
+++ pages/3_Base_de_Donnée.py
@@ -8,7 +8,7 @@ st.header("Présentation des bases de données")
 
 @st.cache_data
 def charger_json():
-    chemin_fichier = "Scrapping\\data_2_all_movie.json"
+    chemin_fichier = "..\\Scrapping\\data_2_all_movie.json"
     df = pd.read_json(chemin_fichier)
     return df
 
@@ -20,7 +20,7 @@ st.write(df)
 
 @st.cache_data
 def charger_json2():
-    chemin_fichier = "Scrapping\\data_commentaire_prov.json"
+    chemin_fichier = "..\\Scrapping\\data_commentaire_prov.json"
     df = pd.read_json(chemin_fichier)
     return df
 
