40ba1ae960d8b9b02a504cfc0148d7d1af8926b6
Basma-G <127091566+Basma-Ghaffour@users.noreply.github.com>
2024-04-02 15:35:52 +0200

    Update 9_Choix_de_film_aléatoirement.py
    lien rep

--- pages/9_Choix_de_film_aléatoirement.py
+++ pages/9_Choix_de_film_aléatoirement.py
@@ -46,7 +46,7 @@ if bouton:
     nombre_aleatoire = random.randint(0, 99)
     ligne_select = df.iloc[nombre_aleatoire]
     with colonne_1:
-        chemin_image= ligne_select["imageRep"]
+        chemin_image= os.path.normpath(ligne_select["imageRep"])
         repertoire=os.getcwd()
         chemin_image=os.path.join(repertoire,chemin_image)
         st.image(chemin_image, caption="", width=325)
